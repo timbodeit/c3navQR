5133553c49b41bd747619e501d3ca507ab59505f
Tim Bodeit <tim@bodeit.com>
2015-12-27 19:45:41 +0100

    Add POI to logo

--- generate.php
+++ generate.php
@@ -21,6 +21,7 @@ div.logo { font-weight: bold; }
 div.logo img, div.logo span { vertical-align: middle; }
 div.logo img { height: 8em; }
 div.logo span { font-size: 5em; }
+div.logo span.light { font-weight: 100; }
 div.generatorLink { text-align: center; margin-top: 80px; font-size: 0.7em; }
 @media print {
   div.page { page-break-before: always; }
@@ -37,7 +38,7 @@ foreach ($keys as $key) {
   } else {
     echo "<div class=\"page\">\n";
   }
-  echo "<div class=\"logo\"><img src=\"https://c3nav.de/static/img/logo.png\"><span>32c3nav</span></div>\n";
+  echo "<div class=\"logo\"><img src=\"https://c3nav.de/static/img/logo.png\"><span>32c3nav</span>&nbsp;&nbsp;&nbsp;<span class=\"light\">POI</span></div>\n";
   $navUrl = urlencode("https://c3nav.de/o$key");
   $imgUrl = "https://chart.googleapis.com/chart?cht=qr&chl=$navUrl&chs=545x545&choe=UTF-8&chld=L|2";
   echo "  <img src=\"$imgUrl\" class=\"qr\">\n";
